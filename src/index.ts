190007c6e023f781d08e502010eeebb008df444b
lucifer1017 <lucifergrey1017@gmail.com>
2025-05-17 15:47:08 +0530

    Initial commit from Create Llama

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { LlamaIndexServer } from "@llamaindex/server";
+import "dotenv/config";
+import { initSettings } from "./app/settings";
+import { workflowFactory } from "./app/workflow";
+
+initSettings();
+
+new LlamaIndexServer({
+  workflow: workflowFactory,
+  uiConfig: {
+    appTitle: "LlamaIndex App",
+    componentsDir: "components",
+    devMode: true,
+  },
+}).start();
